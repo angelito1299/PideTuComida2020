30a0154511fd1214f0504a822896cb8ec2298587
Angel <lopeztomaylla1299@gmail.com>
2020-06-23 17:39:53 -0500

    se agrego el login

--- app/src/main/java/proyecto/pidetucomida/MainActivity.java
+++ app/src/main/java/proyecto/pidetucomida/MainActivity.java
@@ -2,19 +2,26 @@ package proyecto.pidetucomida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+   Button btnSesion,btnRegitrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comentario del lejendario
 
     }
     public void onClick(View view){
+        if(view.getId()==btnSesion.getId()){
+            Intent miIntent = new Intent(MainActivity.this,Login.class);
+            startActivity(miIntent);
+        }else{
+
+        }
 
     }
 
