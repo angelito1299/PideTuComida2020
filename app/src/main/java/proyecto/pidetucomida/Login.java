a12408ba80f28a7d1dd1a1c9127d5810a674f8d8
Angel <lopeztomaylla1299@gmail.com>
2020-06-26 16:06:00 -0500

    26/06/2020 se actualizo

--- app/src/main/java/proyecto/pidetucomida/Login.java
+++ app/src/main/java/proyecto/pidetucomida/Login.java
@@ -11,26 +11,40 @@ import android.widget.Toast;
 
 public class Login extends AppCompatActivity{
  EditText edtUsuario,edtPassword;
+ Button btnLogin,btnOlvide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         edtUsuario = (EditText) findViewById(R.id.edtUsuario);
         edtPassword = (EditText) findViewById(R.id.edtPassword);
+        btnLogin =(Button) findViewById(R.id.btnLogin);
+        btnOlvide =(Button) findViewById(R.id.btnOlvide);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if((edtUsuario.getText().toString().equals("Admin") )&& (edtPassword.getText().toString().equals("1234"))){
+                   Intent intent = new Intent(Login.this, MenuPrincipal.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "contraseña incorrecta", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
 
     }
 
     public void onClick(View v) {
-        Intent intent =null;
-        if(edtUsuario.getText().toString().equals("Hola") && edtPassword.getText().toString().equals("1234")){
-            intent=new Intent(Login.this,MenuPrincipal.class);
+        if(v.getId()==R.id.btnOlvide){
+            Intent intent=new Intent(Login.this,RecuperContra.class);
             startActivity(intent);
-        }else {
-            Toast.makeText(getApplicationContext(), "contraseña incorrecta", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(),"algo falla",Toast.LENGTH_LONG).show();
         }
-    }
 
+    }
     }
 
 
